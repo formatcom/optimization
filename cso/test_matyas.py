81771d2ffc8bf4127bddc53d2a57ab807a157da1
Vinicio Valbuena <vinicio.valbuena89@gmail.com>
2021-04-12 01:13:12 +0200

    update results

--- cso/test_matyas.py
+++ cso/test_matyas.py
@@ -1,11 +1,16 @@
 # REF: https://en.wikipedia.org/wiki/Test_functions_for_optimization
+import os
+
+os.environ['OMP_NUM_THREADS'] = '1'
 from cso import CSO
 
 def matyas_function(x, *args, **kwargs):
     return 0.26 * ( x[0] ** 2 + x[1] ** 2 ) - 0.48 * x[0] * x[1]
 
+
 o = CSO(matyas_function, lb=[-10, -10], ub=[10, 10],
-        n_cats=500, smp=10, maxiter=200, cdc=1, mr=0.5)
+            workers=1, threads=1, maxiter=200,
+            cats=500, mr=0.5, smp=10, cdc=1, srd=0.1, spc=False, omega=0.5)
 
 best = o.run()
 
